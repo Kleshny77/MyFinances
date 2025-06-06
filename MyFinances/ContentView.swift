ceb4e41d8a5d29998e87d9c40b6170d3f8732e6f
Artem <samsonovartem00@gmail.com>
2025-06-06 20:21:43 +0300

    2. Реализовал конвертирование Transaction в json object и обратно

--- MyFinances/ContentView.swift
+++ MyFinances/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
-    }
 }
 
 #Preview {
--- MyFinances/Domain Models/TransactionModel.swift
+++ MyFinances/Domain Models/TransactionModel.swift
@@ -17,3 +17,42 @@ struct Transaction {
     let createdAt: Date
     let updatedAt: Date
 }
+
+extension Transaction {
+    private static let formatter = ISO8601DateFormatter()
+    
+    var jsonObject: Any {
+        return [
+                "id": id,
+                "accountId": accountId,
+                "categoryId": categoryId,
+                "amount": String(describing: amount),
+                "transactionDate": Self.formatter.string(from: transactionDate),
+                "comment": comment,
+                "createdAt": Self.formatter.string(from: createdAt),
+                "updatedAt": Self.formatter.string(from: updatedAt)
+            ]
+    }
+    
+    static func parse(jsonObject: Any) -> Transaction? {
+        guard let dict = jsonObject as? [String: Any],
+              let id: Int = dict["id"] as? Int,
+              let accountId: Int = dict["accountId"] as? Int,
+              let categoryId: Int = dict["categoryId"] as? Int,
+              let amountDouble: String = dict["amount"] as? String,
+              let transactionDateString: String = dict["transactionDate"] as? String,
+              let comment: String = dict["comment"] as? String,
+              let createdAtString: String = dict["createdAt"] as? String,
+              let updatedAtString: String = dict["updatedAt"] as? String
+        else { return nil }
+        
+        guard let amount = Decimal(string: amountDouble) else { return nil }
+        
+        guard let transactionDate = Self.formatter.date(from: transactionDateString),
+              let createdAt = Self.formatter.date(from: createdAtString),
+              let updatedAt = Self.formatter.date(from: updatedAtString)
+        else { return nil }
+        
+        return Transaction(id: id, accountId: accountId, categoryId: categoryId, amount: amount, transactionDate: transactionDate, comment: comment, createdAt: createdAt, updatedAt: updatedAt)
+    }
+}
