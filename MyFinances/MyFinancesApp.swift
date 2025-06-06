36c215fbb7e127da9fd4af14d39d568ecba8ab9b
Artem <samsonovartem00@gmail.com>
2025-06-06 18:50:02 +0300

    Initial Commit

---
+++ MyFinances/MyFinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinancesApp.swift
+//  MyFinances
+//
+//  Created by Артём on 06.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyFinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
