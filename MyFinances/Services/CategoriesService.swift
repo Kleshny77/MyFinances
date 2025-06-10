8ff1d57fa83560ecd7ee39f526390c76cf3b2a18
Artem <samsonovartem00@gmail.com>
2025-06-10 04:50:51 +0300

    Реализован замоканный сервис CategoriesService

---
+++ MyFinances/Services/CategoriesService.swift
@@ -0,0 +1,33 @@
+//
+//  CategoriesService.swift
+//  MyFinances
+//
+//  Created by Артём on 10.06.2025.
+//
+
+import Foundation
+
+struct CategoriesService {
+    func fetchCategories() async throws -> [Category] {
+        let categories = [
+            Category(id: 1, name: "Образование", emoji: "📚", isIncome: false),
+            Category(id: 2, name: "Зарплата", emoji: "💰", isIncome: true),
+            Category(id: 3, name: "Премия", emoji: "🤑", isIncome: true),
+            Category(id: 4, name: "Еда", emoji: "🍕", isIncome: false),
+            Category(id: 5, name: "Коммунальные услуги", emoji: "🔌", isIncome: false)
+        ]
+        
+        return categories
+    }
+    
+    func fetchCategories(direction: Direction) async throws -> [Category] {
+        let categories = try await fetchCategories()
+        
+        switch direction {
+        case .income:
+            return categories.filter { $0.isIncome }
+        case .outcome:
+            return categories.filter { !$0.isIncome }
+        }
+    }
+}
